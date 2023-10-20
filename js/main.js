bbff5ec8059541dc2504e460e6deca1363b596c1
Radyslav Dmytrenko <sheogoradik@gmail.com>
2023-10-20 18:36:48 +0300

    dd_designer site layout version 1.0 ready

--- js/main.js
+++ js/main.js
@@ -48,14 +48,54 @@ window.addEventListener("scroll", () => {
     }
 });
 
-//Portfolio Slider
-const swiper = new Swiper('.graphic-slider', {
+//Tabs
+const tabFirst = document.querySelector('.tab-1');
+const tabSecond = document.querySelector('.tab-2');
+const tabThird = document.querySelector('.tab-3');
+const contentFirst = document.querySelector('.tabs__content_1');
+const contentSecond = document.querySelector('.tabs__content_2');
+const contentThird = document.querySelector('.tabs__content_3');
+
+const tabsList = [tabFirst, tabSecond, tabThird];
+const contentList = [contentFirst, contentSecond, contentThird];
+
+function tabOnClick(e) {
+    let currentTarget = e.target;
+    console.dir(currentTarget);
+
+    tabsList.forEach((tab) => {
+        tab.classList.remove('active');
+    });
+
+    contentList.forEach((content) => {
+        content.classList.remove('active');
+    });
+
+    if (currentTarget === tabFirst) {
+        tabFirst.classList.add('active');
+        contentFirst.classList.add('active');
+    } else if (currentTarget === tabSecond) {
+        tabSecond.classList.add('active');
+        contentSecond.classList.add('active');
+    } else {
+        tabThird.classList.add('active');
+        contentThird.classList.add('active');
+    }
+}
+
+tabsList.forEach((tab) => {
+    tab.addEventListener("click", tabOnClick);
+})
+
+//Sliders
+//Graphic portfolio Slider
+const graphicSlider = new Swiper('.graphic-slider', {
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.graphic-slider-btn.swiper-button-next',
+        prevEl: '.graphic-slider-btn.swiper-button-prev',
     },
     pagination: {
-        el: '.swiper-pagination',
+        el: '.graphic-slider-pagination.swiper-pagination',
         clickable: true,
         dynamicBullets: true,
     },
@@ -68,13 +108,57 @@ const swiper = new Swiper('.graphic-slider', {
         320: {
             slidesPerView: 1.1,
             grid: {
-                rows: 4,
+                rows: 3,
             },
         },
         500: {
             slidesPerView: 2,
             grid: {
-                rows: 4,
+                rows: 3,
+            },
+        },
+        768: {
+            slidesPerView: 1.2,
+            grid: {
+                rows: 2,
+            },
+        },
+        1024: {
+            slidesPerView: 2,
+            grid: {
+                rows: 2,
+            },
+        },
+    },
+});
+
+//Web portfolio Slider
+const webSlider = new Swiper('.web-slider', {
+    navigation: {
+        nextEl: '.web-slider-btn.swiper-button-next',
+        prevEl: '.web-slider-btn.swiper-button-prev',
+    },
+    pagination: {
+        el: '.web-slider-pagination.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    autoHeight: false,
+    slidesPerView: 2,
+    grid: {
+        rows: 2,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1.1,
+            grid: {
+                rows: 3,
+            },
+        },
+        500: {
+            slidesPerView: 2,
+            grid: {
+                rows: 3,
             },
         },
         768: {
